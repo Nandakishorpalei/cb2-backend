76bc842d9dc45c384ca8fe9a57692cba7b0d046c
Nandakishorpalei <nandakishorpalei7676@gmail.com>
2022-01-20 20:48:13 +0530

    completed

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,9 @@
-require("dotenv").config({ path: require("find-config")(".env") });
-const mongoose = require("mongoose");
+require('dotenv').config({ path: require('find-config')('.env') });
+const mongoose = require('mongoose');
 
-const connect = () => {
-  return mongoose.connect(
-    `mongodb+srv://nanda45:${process.env.MONGOPASSWORD}@cluster0.h4li8.mongodb.net/cb2Clone`
-  );
+const connect= ()=>{
+    return mongoose.connect(`mongodb+srv://nanda45:${process.env.MONGOPASSWORD}@cluster0.h4li8.mongodb.net/cb2Clone`)
 };
+
 
 module.exports = connect;
--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
-const express = require("express");
-const ejs = require("ejs");
+require('dotenv').config({ path: require('find-config')('.env') });
+const express = require('express');
+const ejs = require('ejs');
+const passport = require("./config/passport");
+const Razorpay = require('razorpay');
 
 const app = express();
 
@@ -21,40 +24,80 @@ app.use("/cart", cartController);
 const productDescriptionController = require("./controller/productDescriptionController");
 app.use("/productDescription", productDescriptionController);
 
-const checkoutController = require("./controller/checkoutController");
-app.use("/checkoutshipping", checkoutController);
 
-app.get("/", async (req, res) => {
-  try {
-    res.render("home");
-  } catch (e) {
-    res.status(500).send(e.message);
-  }
-});
+const signupController = require('./controller/signupController');
+app.use("/signup", signupController);
 
-app.get("/store", async (req, res) => {
-  try {
-    res.render("storelocation");
-  } catch (e) {
-    res.status(500).send(e.message);
-  }
-});
+const signinController = require('./controller/signinController');
+app.use("/signin", signinController);
 
-app.get("/login", async (req, res) => {
-  try {
-    res.render("login");
-  } catch (e) {
-    res.status(500).send(e.message);
-  }
-});
+const checkoutshippingController = require("./controller/checkoutshippingController");
+app.use("/checkoutshipping", checkoutshippingController);
 
-app.get("/signup", async (req, res) => {
-  try {
-    res.render("signup");
-  } catch (e) {
-    res.status(500).send(e.message);
-  }
-});
+const paymentController = require("./controller/paymentController");
+app.use("/payment", paymentController);
+
+const paymentSuccessController = require("./controller/paymentSuccessController");
+app.use("/paymentsuccess",paymentSuccessController);
+
+//google authentication
+
+app.use(passport.initialize());
+var details ;
+passport.serializeUser(function(user, done){
+    done(null, user);
+    details = user;
+  });
+  
+  passport.deserializeUser(function(user, done){
+      done(null, user);
+  });
+
+
+app.get('/auth/google',
+  passport.authenticate('google', { scope:
+      [ 'email', 'profile' ] }
+));
+
+app.get( '/auth/google/callback',
+    passport.authenticate( 'google', {
+        successRedirect: '/',
+        failureRedirect: '/signup'
+}));
+
+
+
+
+
+app.get("/", async(req,res)=>{
+    try{
+       res.render("home")
+    }catch(e){
+        res.status(500).send(e.message)
+    }
+})
+
+app.get("/store", async(req,res)=>{
+    try{
+       res.render("storelocation")
+    }catch(e){
+        res.status(500).send(e.message)
+    }
+})
+
+
+
+
+// app.get("/products", async(req,res)=>{
+//     try{
+//        const products = await Product.find().lean().exec();
+//        res.render("products",{products:products})
+//     }catch(e){
+//         res.status(500).send(e.message)
+//     }
+// })
+
+
 
 app.get("/newpage", async (req, res) => {
   try {
@@ -64,4 +107,24 @@ app.get("/newpage", async (req, res) => {
   }
 });
 
+
+
+const razorpay=new Razorpay({
+  key_id:process.env.RAZORPAY_ID,
+  key_secret:process.env.RAZORPAY_SECRET
+  })
+  
+  app.post("/order",(req,res)=>{
+  const options = {
+      amount:50000 ,  // amount in the smallest currency unit
+      currency: "INR",
+  
+    };
+    razorpay.orders.create(options, function(err, order) {
+        order_id_var=order.id;
+      res.json(order)
+    });
+  })
+ 
+ 
 module.exports = app;
